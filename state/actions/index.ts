a0bddfa5fe10406b9c6b6965e776223a44cd484e
Brad Rice <bradrice1@gmail.com>
2020-11-23 17:33:32 -0500

    removed the state directory

--- state/actions/index.ts
+++
@@ -1,7 +0,0 @@
-import {REQUEST_ARTWORK_API_DATA, requestArtworkApiData} from './artworkActions';
-import {REQUEST_ARTWORK_DETAIL_API_DATA, requestArtworkDetailApiData} from './artworkActions';
-import {REQUEST_CAROUSEL_API_DATA, requestCarouselApiData} from './carouselActions';
-
-// export REQUEST_API_DATA;
-// export receiveApiData;
-// export requestApiData;
--- state/reducers/images-reducer.spec.ts
+++
@@ -1,8 +0,0 @@
-import reducer from './images-reducer';
-
-describe('Images reducer', () => {
-    test('returns a state object', () => {
-        const result:any = reducer(undefined, {type: 'ANYTHING'})
-        expect(result).toBeDefined()
-    })
-})
