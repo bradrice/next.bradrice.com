b40bd32f050fd09e44b56fbb8b05b330e888ebf8
Brad Rice <bradrice1@gmail.com>
2022-12-15 12:24:54 -0500

    added codelogic and changed styles to portfolio page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -30,9 +30,9 @@ export default function PortfolioPage() {
       <div className="row">
         <div className="col-sm-12">
           <div className={styles.PortfolioPage}>
-            <div className="mt-1 mb-2">
+            <h1 className="mt-1 mb-2">
             Portfolio
-            </div>
+            </h1>
           </div>
         </div>
         </div>
@@ -47,58 +47,111 @@ export default function PortfolioPage() {
 
 <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/changeRequestsPage.png" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('changeRequestsPage.png', 'React development')
+                }} src="images/portfolio/changeRequestsPage.png" />
         <Card.Body>
-            <p className="dscr">Change Requests from Github, in Codelogic</p>
+            <p className={styles.dscr}>React code: Change Requests from Github, in Codelogic</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/multi-select-explorer.png" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('multi-select-explorer.png', 'React development')
+                }} src="images/portfolio/multi-select-explorer.png" />
         <Card.Body>
-            <p className="dscr">Custom built search, with multi-select in CodeLogic.</p>
+            <p className={styles.dscr}>React custom built search, with multi-select in CodeLogic.</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/context-manu.png" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('context-manu.png', 'React development')
+                }} src="images/portfolio/context-manu.png" />
         <Card.Body>
-            <p className="dscr">Custom built context menu inside of CodeLogic app.</p>
+            <p className={styles.dscr}>React development CodeLogic app.</p>
         </Card.Body>
     </Card>
     </div>
     </div>
-        
+
+    <div className="row">
+    <h2 className="large-12 columns">Mobile App Development</h2>
+    </div>
+
+    <div className="row">
+        <div className="col-sm-4">
+            <Card className={styles.cardItem}>
+                <Card.Img className={styles.cardImage} onClick={()=>{
+                    handleClick('tumobile-loginscreen.jpg', "Mobile app development")
+                    }} src="images/portfolio/tumobile-loginscreen.jpg" />
+                <Card.Body>
+                <p className={styles.dscr}>Mobile app - Tooling U-SME class player</p>
+                </Card.Body>
+            </Card>				
+        </div>
+
+        <div className="col-sm-4">
+            <Card className={styles.cardItem}>
+                <Card.Img className={styles.cardImage} onClick={()=>{
+                    handleClick('tumobile-studentlist.jpg', "Mobile app development")
+                    }} src="images/portfolio/tumobile-studentlist.jpg" />
+                <Card.Body>
+                <p className={styles.dscr}>Mobile app - Tooling U-SME class player</p>
+                </Card.Body>
+            </Card>				
+        </div>
+
+        <div className="col-sm-4">
+            <Card className={styles.cardItem}>
+                <Card.Img className={styles.cardImage} onClick={()=>{
+                    handleClick('tumobile-class.jpg', "Mobile app development")
+                    }} src="images/portfolio/tumobile-class.jpg" />
+                <Card.Body>
+                <p className={styles.dscr}>Mobile app - Tooling U-SME class player</p>
+                </Card.Body>
+            </Card>				
+        </div>
+    </div>
+    <div className="row">
+    <h2 className="large-12 columns">Web Development</h2>
+    </div>
 
 <div className="row">
 
 <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/UA_Home_thumb.jpg" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('UA_Home.png', 'React development')
+                }} src="images/portfolio/UA_Home_thumb.jpg" />
         <Card.Body>
-            <p className="dscr">University of Akron Home page, responsive for mobile devices</p>
+            <p className={styles.dscr}>University of Akron Home page, responsive for mobile devices</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/curr_guide_thumb.jpg" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('curr_guide.jpg', 'HTML')
+                }} src="images/portfolio/curr_guide_thumb.jpg" />
         <Card.Body>
-            <p className="dscr">Custom built responsive site for Prospective students visiting UA searching for majors offered.</p>
+            <p className={styles.dscr}>Custom built responsive site for Prospective students visiting UA searching for majors offered.</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-    <Card.Img className={styles.cardImage} src="images/portfolio/curriculum-search_thumb.png" />
+    <Card.Img className={styles.cardImage} onClick={()=>{
+                handleClick('curriculum-search.png', 'Angular JS')
+                }} src="images/portfolio/curriculum-search_thumb.png" />
         <Card.Body>
-            <p className="dscr">Custom built responsive site for Prospective students visiting UA searching for classes available.</p>
+            <p className={styles.dscr}>Custom built responsive site for Prospective students visiting UA searching for classes available.</p>
         </Card.Body>
     </Card>
     </div>
@@ -113,36 +166,33 @@ export default function PortfolioPage() {
 <div className="row">
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-        <Card.Img className={styles.cardImage} src="images/portfolio/ncb_promo_thumb.jpg" />
-        <Card.Body>
-            <p className="dscr">National City bank offer for car loans that gave away gas gift cards when taking out a loan. Photoshop technique.</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+        <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('ncb_promo.jpg', 'Photoshop technique')
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/ncb_promo_thumb.jpg" />
+        <Card.Body>
+            <p className={styles.dscr}>National City bank offer for car loans that gave away gas gift cards when taking out a loan. Photoshop technique.</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-        <Card.Img className={styles.cardImage} src="images/portfolio/uh_laurelwood_1_thumb.jpg" />
-        <Card.Body>
-        <p className="dscr">Brochure cover to promote University Health Systems Laurelwood Hospital pyschological services. Photoshop technique.</p>
-        <Button variant="primary" size="sm" onClick={()=>{
+        <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('uh_laurelwood_1.jpg', 'Photoshop technique')
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/uh_laurelwood_1_thumb.jpg" />
+        <Card.Body>
+        <p className={styles.dscr}>Brochure cover to promote University Health Systems Laurelwood Hospital pyschological services. Photoshop technique.</p>
         </Card.Body>
     </Card>
     </div>
 
     <div className="col-sm-4">
     <Card className={styles.cardItem}>
-        <Card.Img className={styles.cardImage} src="images/portfolio/gl_science_thumb.jpg" />
-        <Card.Body>
-        <p className="dscr">The Annual Report, 1999 for Great Lakes Science Center.</p>
-        <Button variant="primary" size="sm" onClick={()=>{
+        <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('gl_science.jpg', "Brochure design and layout")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/gl_science_thumb.jpg" />
+        <Card.Body>
+        <p className={styles.dscr}>The Annual Report, 1999 for Great Lakes Science Center.</p>
         </Card.Body>
     </Card>
         </div>
@@ -155,36 +205,33 @@ export default function PortfolioPage() {
 <div className="row">
     <div className="col-sm-4">
         <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/lodi_chamber_thumb.png" />
-            <Card.Body>
-            <p className="dscr">Logo design - Village of Lodi Chamber of Commerce</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('lodi_chamber.png', "Logo design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/lodi_chamber_thumb.png" />
+            <Card.Body>
+            <p className={styles.dscr}>Logo design - Village of Lodi Chamber of Commerce</p>
             </Card.Body>
         </Card>				
     </div>
 
     <div className="col-sm-4">
         <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/mt25.png" />
-            <Card.Body>
-            <p className="dscr">Logo design - Faith based social services.</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('mt25.png', "Logo design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/mt25.png" />
+            <Card.Body>
+            <p className={styles.dscr}>Logo design - Faith based social services.</p>
             </Card.Body>
         </Card>	      
    </div>
 
     <div className="col-sm-4">
         <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/online_learners_thumb.jpg" />
-            <Card.Body>
-            <p className="dscr">Icon - The University of Akron Online Learners web application.</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('online_learners.jpg', "Icon design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/online_learners_thumb.jpg" />
+            <Card.Body>
+            <p className={styles.dscr}>Icon - The University of Akron Online Learners web application.</p>
             </Card.Body>
         </Card>	
     </div>
@@ -193,81 +240,38 @@ export default function PortfolioPage() {
 <div className="row">
 	<div className="col-sm-4">
         <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/quickpass_thumb.jpg" />
-            <Card.Body>
-            <p className="dscr">Icon design - Quickpass Macintosh application</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('quickpass.jpg', "Icon design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/quickpass_thumb.jpg" />
+            <Card.Body>
+            <p className={styles.dscr}>Icon design - Quickpass Macintosh application</p>
             </Card.Body>
         </Card>	
     </div>
 				
     <div className="col-sm-4">
         <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/Z-alert_500.png" />
-            <Card.Body>
-            <p className="dscr">Icon design - Z-Alert text messaging application</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('Z-alert_500.png', "Icon design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/Z-alert_500.png" />
+            <Card.Body>
+            <p className={styles.dscr}>Icon design - Z-Alert text messaging application</p>
             </Card.Body>
         </Card>	
 
     </div>
 			<div className="col-sm-4">
             <Card className={styles.cardItem}>
-            <Card.Img className={styles.cardImage} src="images/portfolio/golden_ideas_thumb.jpg" />
-            <Card.Body>
-            <p className="dscr">Icon design - Golden Ideas web application</p>
-            <Button variant="primary" size="sm" onClick={()=>{
+            <Card.Img className={styles.cardImage} onClick={()=>{
                 handleClick('golden_ideas.jpg', "Icon design")
-                }}>View Larger Image</Button>
+                }} src="images/portfolio/golden_ideas_thumb.jpg" />
+            <Card.Body>
+            <p className={styles.dscr}>Icon design - Golden Ideas web application</p>
             </Card.Body>
         </Card>
     </div>
 
-    <div className="row">
-    <h2 className="large-12 columns">Mobile App Development</h2>
-    </div>
-
-    <div className="row">
-        <div className="col-sm-4">
-            <Card className={styles.cardItem}>
-                <Card.Img className={styles.cardImage} src="images/portfolio/tumobile-loginscreen.jpg" />
-                <Card.Body>
-                <p className="dscr">Mobile app - Tooling U-SME class player</p>
-                <Button variant="primary" size="sm" onClick={()=>{
-                    handleClick('tumobile-loginscreen.jpg', "Mobile app development")
-                    }}>View Larger Image</Button>
-                </Card.Body>
-            </Card>				
-        </div>
-
-        <div className="col-sm-4">
-            <Card className={styles.cardItem}>
-                <Card.Img className={styles.cardImage} src="images/portfolio/tumobile-studentlist.jpg" />
-                <Card.Body>
-                <p className="dscr">Mobile app - Tooling U-SME class player</p>
-                <Button variant="primary" size="sm" onClick={()=>{
-                    handleClick('tumobile-studentlist.jpg', "Mobile app development")
-                    }}>View Larger Image</Button>
-                </Card.Body>
-            </Card>				
-        </div>
-
-        <div className="col-sm-4">
-            <Card className={styles.cardItem}>
-                <Card.Img className={styles.cardImage} src="images/portfolio/tumobile-class.jpg" />
-                <Card.Body>
-                <p className="dscr">Mobile app - Tooling U-SME class player</p>
-                <Button variant="primary" size="sm" onClick={()=>{
-                    handleClick('tumobile-class.jpg', "Mobile app development")
-                    }}>View Larger Image</Button>
-                </Card.Body>
-            </Card>				
-        </div>
-    </div>
+    
 </div>
 </div>
 </div>
