5f91ebfe4ddbaff27ceaf10ddf2c1db1176b2d69
Brad Rice <bradrice1@gmail.com>
2024-12-17 11:43:52 -0500

    no-store cache on home page

--- lib/api.ts
+++ lib/api.ts
@@ -6,7 +6,7 @@ export const fetchArtworkData = async () => {
         return data;
     } catch (e) {
         console.log(e);
-    } 
+    }
 }
 
 export const fetchArtworkDetailData = async (id) => {
@@ -18,38 +18,38 @@ export const fetchArtworkDetailData = async (id) => {
         return data;
     } catch (e) {
         console.log(e);
-    } 
+    }
 }
 
-export const fetchArtworkPagedData = async (url:string) => {
+export const fetchArtworkPagedData = async (url: string) => {
     console.log(url);
     try {
-        const response = await fetch(url+'&format=json');
+        const response = await fetch(url + '&format=json');
         const data = (await response).json();
         // console.log(data);
         return data;
     } catch (e) {
         console.log(e);
-    } 
+    }
 }
 
 export const fetchCarouselData = async () => {
     try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_REACT_APP_API_SERVER}/api/carousel/1`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_REACT_APP_API_SERVER}/api/carousel/1`, { cache: 'no-store' });
         const data = await response.json();
         console.log(data);
         return data;
     } catch (e) {
         console.log(e);
     }
-    
+
 }
 
 
 export const fetchArtworkMediaTypes = async () => {
     try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_REACT_APP_API_SERVER}/api/artwork/mediatypes`);
-    const data = (await response).json();
+        const response = await fetch(`${process.env.NEXT_PUBLIC_REACT_APP_API_SERVER}/api/artwork/mediatypes`);
+        const data = (await response).json();
         console.log(data);
         return data;
     } catch (e) {
